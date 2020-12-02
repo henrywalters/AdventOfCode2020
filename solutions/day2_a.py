4d91f3c9c429d1ac5d4cb9059a98c0536c114f44
Henry Walters <henrywalters20@gmail.com>
2020-12-02 01:05:10 -0500

    Day 2 solution A

--- solutions/day2_a.py
+++ solutions/day2_a.py
@@ -11,11 +11,36 @@ timer = Timer()
 
 # Write your code here
 
+def parse_line(line):
+    parts = line.split(':')
+    policyParts = parts[0].split(' ')
+    quantParts = policyParts[0].split('-')
+
+    return {
+        'password': parts[1].strip(),
+        'policy': {
+            'letter': policyParts[1],
+            'minCount': int(quantParts[0]),
+            'maxCount': int(quantParts[1])
+        }   
+    }
+
+def is_valid_password(policy, password):
+    letter_matches = 0
+    for c in password:
+        if c == policy['letter']:
+            letter_matches += 1
+    return policy['minCount'] <= letter_matches <= policy['maxCount']
+
 with open('files/day2.txt', 'r') as f:
     lines = f.readlines()
+    valid_passwords = 0
     for line in lines:
-        print(line)
+        parsed = parse_line(line)
+        if is_valid_password(parsed['policy'], parsed['password']):
+            valid_passwords += 1
 
+print("There are {f} valid passwords".format(f=valid_passwords))
 
 # And stop here
 
